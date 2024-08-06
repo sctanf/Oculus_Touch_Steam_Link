fa8983f229c87ae707bf6213488c3c88f86cba70
sctanf <36978460+sctanf@users.noreply.github.com>
2024-08-06 08:07:57 -0500

    remove hsb

--- CustomHMD/TouchControllerDriver.h
+++ CustomHMD/TouchControllerDriver.h
@@ -201,42 +201,6 @@ public:                                                                         
 
         return VRInitError_None;
     }
-    enum HandSkeletonBone : size_t
-    {
-        HSB_Root = 0U,
-        HSB_Wrist,
-        HSB_Thumb0,
-        HSB_Thumb1,
-        HSB_Thumb2,
-        HSB_Thumb3, // Last, no effect
-        HSB_IndexFinger0,
-        HSB_IndexFinger1,
-        HSB_IndexFinger2,
-        HSB_IndexFinger3,
-        HSB_IndexFinger4, // Last, no effect
-        HSB_MiddleFinger0,
-        HSB_MiddleFinger1,
-        HSB_MiddleFinger2,
-        HSB_MiddleFinger3,
-        HSB_MiddleFinger4, // Last, no effect
-        HSB_RingFinger0,
-        HSB_RingFinger1,
-        HSB_RingFinger2,
-        HSB_RingFinger3,
-        HSB_RingFinger4, // Last, no effect
-        HSB_PinkyFinger0,
-        HSB_PinkyFinger1,
-        HSB_PinkyFinger2,
-        HSB_PinkyFinger3,
-        HSB_PinkyFinger4, // Last, no effect
-        HSB_Aux_Thumb,
-        HSB_Aux_IndexFinger,
-        HSB_Aux_MiddleFinger,
-        HSB_Aux_RingFinger,
-        HSB_Aux_PinkyFinger,
-
-        HSB_Count
-    };
     virtual void Deactivate()
     {
         m_unObjectId = vr::k_unTrackedDeviceIndexInvalid;
